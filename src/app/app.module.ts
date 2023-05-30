4c6f89da1cd0d3f4fe1010777c01a9e0b4373b2c
Yakov Zhavoronkov <yakov.zhavoronkov@jelvix.com>
2023-05-30 20:44:18 +0300

    add husky

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { MAPBOX_ACCESS_TOKEN, getMapboxAccessToken } from 'src/mapbox-config';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from 'src/environments/environment';
-import * as fromApp from './store';
+import { MAPBOX_ACCESS_TOKEN, getMapboxAccessToken } from 'src/mapbox-config';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app.component';
+import * as fromApp from './store';
 
 @NgModule({
   declarations: [AppComponent],
