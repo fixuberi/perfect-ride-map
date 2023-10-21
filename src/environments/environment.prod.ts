68495b52d598e923421d85090a99462493ec92cd
Yakov Zhavoronkov <yakov.zhavoronkov@jelvix.com>
2023-10-21 18:10:02 +0300

    clear env files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,10 @@
-const BACKEND_URL = 'https://perfect-ride-map-553dd9cef958.herokuapp.com';
+const BACKEND_URL = '';
 
 export const environment = {
   production: true,
   DOMAIN: 'http://localhost:4200',
   API_DOMAIN: `${BACKEND_URL}/api`,
   mapbox: {
-    accessToken:
-      'pk.eyJ1IjoiZml4MTExIiwiYSI6ImNsaGtjNDNtZDBxOXIzdG9lOTFxcXlqaDEifQ.658zn0GgDVhD-MimEu3sZg',
+    accessToken: '',
   },
 };
